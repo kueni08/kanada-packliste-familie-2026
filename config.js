322804caa0420afcd3decb8a333c7efcf31c3413
kueni08 <marc.kueni@gmail.com>
2026-07-15 08:56:03 +0200

    config.js hinzufügen

---
+++ config.js
@@ -0,0 +1,5 @@
+// Wird bei der Einrichtung mit den öffentlichen Supabase-Projektdaten befüllt.
+window.APP_CONFIG = {
+  supabaseUrl: "https://idfxcrriztictfxqbskw.supabase.co",
+  supabaseAnonKey: "sb_publishable_trpJU3PP8L1KT9dTovDdDA_rzyk1wpj"
+};
